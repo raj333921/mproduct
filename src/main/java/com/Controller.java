1914b4782a33ac4ca908162eacaedf0154cf1ac6
rajeshvarikuntla <raj333921@gmail.com>
2020-06-03 02:15:49 +0530

    Added build version

---
+++ src/main/java/com/Controller.java
@@ -0,0 +1,16 @@
+package com;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/EProduct")
+public class EmployeeProductController {
+
+    @ResponseBody
+    public String signUpCompanyDetails() {
+        return "HelloWorld"
+
+    }
+
+    }
